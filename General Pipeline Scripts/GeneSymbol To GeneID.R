1c1ec11d84d35f2404cb74dda665912757277a94
benjibromberg <122861469+benjibromberg@users.noreply.github.com>
2023-04-25 16:42:55 -0400

    Investigation 3 Updates

--- General Pipeline Scripts/GeneSymbol To GeneID.R
+++ General Pipeline Scripts/GeneSymbol To GeneID.R
@@ -16,10 +16,10 @@ add_GeneID_to_df <- function(df_from_DESeq) {
   print(paste0("The number of rows using gene symbol is ",nrow(df_res_symbol)))
   print(paste0("The number of rows using gene ID is ",nrow(df_res_GeneID)))
   
-  output_add_GeneID_to_df <- list(df_res_symbol = df_res_symbol, 
-                                  df_res_symbol_GeneID = df_res_symbol_GeneID, 
-                                  df_res_GeneID = df_res_GeneID,
-                                  df_res_GeneID_for_pathway = 
-                                    df_res_GeneID_for_pathway)
-  return(output_add_GeneID_to_df)
+  output_df <- list(df_res_symbol = df_res_symbol, 
+                    df_res_symbol_GeneID = df_res_symbol_GeneID, 
+                    df_res_GeneID = df_res_GeneID,
+                    df_res_GeneID_for_pathway = 
+                    df_res_GeneID_for_pathway)
+  return(output_df)
 }
